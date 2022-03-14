98d31fd55276cd74a0737f110f92db1a69aa364b
Mostafa sayed <ms519197@email.com>
2022-03-14 14:03:15 +0200

    second  step

--- commentsOnlyJS/app.js
+++ commentsOnlyJS/app.js
@@ -1,5 +1,6 @@
 // Personal API Key for OpenWeatherMap API
-
+const myApiKey = "71a6b112f43d7bafdd1be3e60eabbb0b";
+//https://api.openweathermap.org/data/2.5/weather?zip=89883,us&appid=71a6b112f43d7bafdd1be3e60eabbb0b&units=metric
 // Event listener to add function to existing HTML DOM element
 
 /* Function called by event listener */
@@ -7,6 +8,5 @@
 /* Function to GET Web API Data*/
 
 /* Function to POST data */
-
 
 /* Function to GET Project Data */
--- server.js
+++ server.js
@@ -2,18 +2,25 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require("express");
+const cors = require("cors");
 // Start up an instance of app
-
+const app = express();
+const port = 3000;
 /* Middleware*/
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+app.listen(port, () => {
+    console.log(`server is running on port:  ${port}`);
+});
