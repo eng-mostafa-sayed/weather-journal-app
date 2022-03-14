8f5350191d100ad09bad2471bd1331f5fb1e4fce
Mostafa sayed <ms519197@email.com>
2022-03-14 20:52:43 +0200

    working on the js file

--- website/app.js
+++ website/app.js
@@ -6,9 +6,8 @@ let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
 //storing my api key and api url
 const myApiKey = "71a6b112f43d7bafdd1be3e60eabbb0b";
-let weatherApi = `https://api.openweathermap.org/data/2.5/weather?zip=${2112},us&appid=${myApiKey}&units=metric`;
 
-// get the button
+// get the button element
 let genBtn = document.querySelector("#generate");
 //this is for testing only
 console.log(genBtn);
@@ -16,14 +15,25 @@ console.log(genBtn);
 genBtn.addEventListener("click", function() {
     //this is for testing only
     console.log("click");
-    //go to the input to take the entered zip code
+    //go to the inputed zip to take the entered zip code
     let zipCode = document.getElementById("zip").value;
     //this is for testing only
     console.log(zipCode);
-    //checking if the user inserted zip code
+
+    //this section for checking if the user inserted zip code
+    //here check if it empty
     if (!zipCode) {
         alert("enter the zip code");
-    } else {
-        console.log("there is zip code");
     }
-});
+
+    //here check if it number not string(string not valid btw)
+    else if (!Number(zipCode)) {
+        alert("zip code must be numeric");
+    }
+    //here it is not abuse or empty
+    else {
+        console.log("there is zip code");
+        let weatherApi = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${myApiKey}&units=metric`;
+        console.log(weatherApi);
+    }
+}); //the end of the event listener
