24516038dc18abc45c2d6bc66a85048a8c0f3e1c
Mostafa sayed <ms519197@email.com>
2022-03-14 19:30:14 +0200

    css

--- commentsOnlyJS/app.js
+++ commentsOnlyJS/app.js
@@ -1,6 +1,5 @@
 // Personal API Key for OpenWeatherMap API
-const myApiKey = "71a6b112f43d7bafdd1be3e60eabbb0b";
-//https://api.openweathermap.org/data/2.5/weather?zip=89883,us&appid=71a6b112f43d7bafdd1be3e60eabbb0b&units=metric
+
 // Event listener to add function to existing HTML DOM element
 
 /* Function called by event listener */
--- website/app.js
+++ website/app.js
@@ -2,4 +2,17 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+const myApiKey = "71a6b112f43d7bafdd1be3e60eabbb0b";
+let weatherApi = `https://api.openweathermap.org/data/2.5/weather?zip=${2112},us&appid=${myApiKey}&units=metric`;
+
+// Event listener to add function to existing HTML DOM element
+let genBtn = document.querySelector("#generate");
+console.log(genBtn);
+genBtn.addEventListener("click", function() {
+    console.log("click");
+    //go to the input to take the entered zip code
+    let zipCode = document.getElementById("zip").value;
+    console.log(zipCode);
+});
